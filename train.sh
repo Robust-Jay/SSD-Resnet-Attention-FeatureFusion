6bd985a9b766fc0779de7fe296d173631024a2e2
withoutyourself <lh1651315311@outlook.com>
2020-03-19 19:28:16 +0800

    add train.sh

---
+++ train.sh
@@ -0,0 +1,4 @@
+python train.py --config-file configs/SSD512/R-101-cbam.yaml
+python train.py --config-file configs/SSD512/R-101-fusion-v1.yaml
+python train.py --config-file configs/SSD512/AC-R-101-cbam.yaml
+python train.py --config-file configs/SSD512/AC-R-101-fusion-v1.yaml
