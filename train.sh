ba44a197272bcb0c63bb937fba27a0d0c5b6d10d
withoutyourself <lh1651315311@outlook.com>
2020-03-21 08:40:49 +0800

    congifs

--- train.sh
+++ train.sh
@@ -2,3 +2,6 @@ python train.py --config-file configs/SSD512/R-101-cbam.yaml
 python train.py --config-file configs/SSD512/R-101-fusion-v1.yaml
 python train.py --config-file configs/SSD512/AC-R-101-cbam.yaml
 python train.py --config-file configs/SSD512/AC-R-101-fusion-v1.yaml
+python train.py --config-file configs/SSD512/R-50-SE.yaml
+python train.py --config-file configs/SSD512/R-50-fusion-v1.yaml
+python train.py --config-file configs/SSD512/R-50-cbam.yaml
